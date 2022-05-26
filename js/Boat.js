47e9b4c0d91e4c1af659aec7d1e1ec061ade0696
edwedja <edwedja_lima@hotmail.com>
2022-05-26 12:57:59 -0300

    aula 27

--- js/Boat.js
+++ js/Boat.js
@@ -1,14 +1,19 @@
 class Boat {
-    constructor(x, y, width, height, boatPos) {
+    constructor(x, y, width, height, boatPos, boatAnimation) {
     
       this.body = Bodies.rectangle(x, y, width, height);
       this.width = width;
       this.height = height;
-  
+      this.speed = 0.05;
       this.image = loadImage("./assets/boat.png");
       this.boatPosition = boatPos;
+      this.animation = boatAnimation;
       World.add(world, this.body);
     }
+    animate(){
+      this.speed += 0.05;
+    }
+  
   remove(i){
     setTimeout(() => {
       Matter.World.remove(world,boates[i].body)
@@ -19,12 +24,13 @@ class Boat {
     display() {
       var angle = this.body.angle;
       var pos = this.body.position;
+      var i= floor (this.speed % this.animation.length)
   
       push();
       translate(pos.x, pos.y);
       rotate(angle);
       imageMode(CENTER);
-      image(this.image, 0, this.boatPosition, this.width, this.height);
+      image(this.animation[i], 0, this.boatPosition, this.width, this.height);
       pop();
     }
   }
